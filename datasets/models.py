12e5cf034202d575ebc1a2a10b6e458985664f82
legalyuzakiexport-debug <legalyuzakiexport@gmail.com>
2026-05-12 15:30:50 +0100

    models.py

--- datasets/models.py
+++ datasets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Dataset(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(blue=True, null=True, verbose_name="Descrição")
+    category = models.CharField(max_length=100, blank=True)
+    file_upload = models.FileField(upload_to='uploads/datasets/', store_full_path=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
