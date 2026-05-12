1ba090dd7fc5e9f05e475ca167b376447f4ef703
legalyuzakiexport-debug <legalyuzakiexport@gmail.com>
2026-05-12 15:43:10 +0100

    models.py

--- datasets/models.py
+++ datasets/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Dataset(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
-    description = models.TextField(blue=True, null=True, verbose_name="Descrição")
+    description = models.TextField(blank=True, null=True, verbose_name="Descrição")
     category = models.CharField(max_length=100, blank=True)
-    file_upload = models.FileField(upload_to='uploads/datasets/', store_full_path=True)
+    file_upload = models.FileField(upload_to='datasets/', verbose_name="Ficheiro")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
     updated_at = models.DateTimeField(auto_now=True)
 
