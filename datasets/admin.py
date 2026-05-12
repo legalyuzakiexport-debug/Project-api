18be8528b9aa6e956203cfac974ff78efbcbbc38
legalyuzakiexport-debug <legalyuzakiexport@gmail.com>
2026-05-12 15:32:51 +0100

    admin.py

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Dataset
 
-# Register your models here.
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+    search_fields = ('title', 'category')
+    list_filter = ('created_at', 'category')
